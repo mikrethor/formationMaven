6bfcea6a031583ca36465fffd69b60e452252099
mikrethor <mikrethor@gmail.com>
2018-06-28 22:39:49 -0400

    Initial commit

---
+++ 002-dependencies/src/test/java/logger/LoggerTest.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.Test;
+
+public class LoggerTest{
+
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void testLogger() {
+        logger.debug("hello");
+    }
+}
